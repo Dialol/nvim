dbb0b078c5657998f7cb5c30c969a8ee876230b0
Maxim Kostrov <diabolik_sl@mail.ru>
2025-04-27 23:37:31 +0300

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,9 +3,38 @@ local lspconfig = require'lspconfig'
 -- local cmp = require'cmp'
 local luasnip = require'luasnip'
 
+-- Определяем функцию on_attach
+local on_attach = function(client, bufnr)
+  -- Включаем подсветку синтаксиса от LSP
+  if client.server_capabilities.documentHighlightProvider then
+    vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+    vim.api.nvim_clear_autocmds { group = "lsp_document_highlight", buffer = bufnr }
+    vim.api.nvim_create_autocmd("CursorHold", {
+      callback = vim.lsp.buf.document_highlight,
+      buffer = bufnr,
+      group = "lsp_document_highlight",
+      desc = "Document Highlight",
+    })
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      callback = vim.lsp.buf.clear_references,
+      buffer = bufnr,
+      group = "lsp_document_highlight",
+      desc = "Clear All the References",
+    })
+  end
+end
+
 -- Настройка для Python
 lspconfig.pyright.setup{
   on_attach = on_attach,
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic",
+        diagnosticMode = "workspace",
+      }
+    }
+  }
 }
 
 -- Настройка для Bash
@@ -23,4 +52,17 @@ lspconfig.html.setup{
   on_attach = on_attach,
 }
 
+-- Настройка диагностики
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
 
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
